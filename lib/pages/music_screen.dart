61be8dbcdeff0beeb3d5cb720f2928533d155004
Saumil <saumil21097@iiitd.ac.in>
2024-03-19 15:15:23 +0530

    music player started

--- lib/pages/music_screen.dart
+++ lib/pages/music_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 import 'package:just_audio/just_audio.dart';
@@ -10,6 +12,10 @@ class MusicScreen extends StatefulWidget {
   @override
   State<MusicScreen> createState() => _MusicScreenState();
 
+}
+
+
+class _MusicScreenState extends State<MusicScreen> {
   String musicUrl = "";
   String thumbnail = "";
 
@@ -38,20 +44,143 @@ class MusicScreen extends StatefulWidget {
     await player.pause();
   }
 
+  @override
   void initState() {
     loadMusic();
     super.initState();
   }
 
+  @override
   void dispose() {
     player.dispose();
     super.dispose();
   }
-
-
-class _MusicScreenState extends State<MusicScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Music Player"),
+      ),
+      body: Column(
+        children: [
+          const Spacer(
+            flex: 2,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              thumbnail,
+              height: 350,
+              width: 350,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: StreamBuilder(
+                stream: player.positionStream,
+                builder: (context, snapshot1) {
+                  final Duration duration = loaded
+                      ? snapshot1.data as Duration
+                      : const Duration(seconds: 0);
+                  return StreamBuilder(
+                      stream: player.bufferedPositionStream,
+                      builder: (context, snapshot2) {
+                        final Duration bufferedDuration = loaded
+                            ? snapshot2.data as Duration
+                            : const Duration(seconds: 0);
+                        return SizedBox(
+                          height: 30,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            child: ProgressBar(
+                              progress: duration,
+                              total:
+                              player.duration ?? const Duration(seconds: 0),
+                              buffered: bufferedDuration,
+                              timeLabelPadding: -1,
+                              timeLabelTextStyle: const TextStyle(
+                                  fontSize: 14, color: Colors.black),
+                              progressBarColor: Colors.red,
+                              baseBarColor: Colors.grey[200],
+                              bufferedBarColor: Colors.grey[350],
+                              thumbColor: Colors.red,
+                              onSeek: loaded
+                                  ? (duration) async {
+                                await player.seek(duration);
+                              }
+                                  : null,
+                            ),
+                          ),
+                        );
+                      });
+                }),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const SizedBox(
+                width: 10,
+              ),
+              IconButton(
+                  onPressed: loaded
+                      ? () async {
+                    if (player.position.inSeconds >= 10) {
+                      await player.seek(Duration(
+                          seconds: player.position.inSeconds - 10));
+                    } else {
+                      await player.seek(const Duration(seconds: 0));
+                    }
+                  }
+                      : null,
+                  icon: const Icon(Icons.fast_rewind_rounded)),
+              Container(
+                height: 50,
+                width: 50,
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle, color: Colors.red),
+                child: IconButton(
+                    onPressed: loaded
+                        ? () {
+                      if (playing) {
+                        pauseMusic();
+                      } else {
+                        playMusic();
+                      }
+                    }
+                        : null,
+                    icon: Icon(
+                      playing ? Icons.pause : Icons.play_arrow,
+                      color: Colors.white,
+                    )),
+              ),
+              IconButton(
+                  onPressed: loaded
+                      ? () async {
+                    if (player.position.inSeconds + 10 <=
+                        player.duration!.inSeconds) {
+                      await player.seek(Duration(
+                          seconds: player.position.inSeconds + 10));
+                    } else {
+                      await player.seek(const Duration(seconds: 0));
+                    }
+                  }
+                      : null,
+                  icon: const Icon(Icons.fast_forward_rounded)),
+              const SizedBox(
+                width: 10,
+              ),
+            ],
+          ),
+          const Spacer(
+            flex: 2,
+          )
+        ],
+      ),
+    );
   }
 }
